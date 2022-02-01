b1833e8a7a36069e7e60b565f3485e567b9e9b10
akosuadenell <29132813+akosuadenell@users.noreply.github.com>
2022-02-01 17:35:29 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {
+variable "TF_VAR_aws_access_key" {
     description = "AWS Access Key"
 }
 
-variable "aws_secret_key" {
+variable "TF_VAR_aws_secret_key" {
     description = "AWS Secret Key"
 }
 
