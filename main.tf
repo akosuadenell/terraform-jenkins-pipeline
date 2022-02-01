f5eca92c61038f45a6a72fa94ee8935360bfc034
akosuadenell <29132813+akosuadenell@users.noreply.github.com>
2022-02-01 17:48:54 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = env.aws_access_key
-    secret_key = env.aws_secret_access_key
-    token = env.aws_session_token
+    #access_key = env.aws_access_key
+    #secret_key = env.aws_secret_access_key
+    #token = env.aws_session_token
     region = var.aws_region
 }
